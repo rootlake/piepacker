181179811fb89361c8a5cd811bfb5d552d224320
rootlake <rootlake@gmail.com>
2025-04-18 22:02:58 -0400

    Refactor: Update pie names (Mince, Peach Tart)

--- src/pies.ts
+++ src/pies.ts
@@ -7,13 +7,13 @@ export interface Pie {
 
 // Define pies in size order
 export const pies: Pie[] = [
-  { name: "Raspberry Mini", radius: 40, assetKey: "raspberry_mini" },
-  { name: "Lemon Tart", radius: 50, assetKey: "lemon_tart" },
-  { name: "Apple Cross", radius: 60, assetKey: "apple_cross" },
-  { name: "Blueberry Pie", radius: 70, assetKey: "blueberry" },
-  { name: "Cherry Pie", radius: 75, assetKey: "cherry_cross" },
+  { name: "Mince Pie", radius: 20, assetKey: "mince" },
+  { name: "Peach Tart", radius: 30, assetKey: "peach_tart" },
+  { name: "Apple Cross", radius: 40, assetKey: "apple_cross" },
+  { name: "Blueberry Pie", radius: 50, assetKey: "blueberry" },
+  { name: "Cherry Pie", radius: 60, assetKey: "cherry_cross" },
   { name: "Chocolate Cream Pie", radius: 80, assetKey: "chocolate_cream" },
-  { name: "Custard Pie", radius: 85, assetKey: "custard" },
+  { name: "Custard Pie", radius: 80, assetKey: "custard" },
   { name: "Key Lime Pie", radius: 90, assetKey: "key_lime" },
   { name: "Lemon Meringue Pie", radius: 95, assetKey: "lemon_meringue" },
   { name: "Oreo Pie", radius: 100, assetKey: "oreo" },
@@ -23,6 +23,6 @@ export const pies: Pie[] = [
   { name: "Strawberry Rhubarb Pie", radius: 120, assetKey: "strawberry_rhubarb" },
   { name: "Tomato Pie", radius: 125, assetKey: "tomato" },
   { name: "Tollhouse Cookie Pie", radius: 130, assetKey: "tollhouse" },
-  { name: "Chicken Pot Pie", radius: 140, assetKey: "chicken" },
-  { name: "Pizza Pie", radius: 150, assetKey: "pizza_pie" },
+  { name: "Chicken Pot Pie", radius: 135, assetKey: "chicken" },
+  { name: "Pizza Pie", radius: 140, assetKey: "pizza_pie" },
 ]; 
